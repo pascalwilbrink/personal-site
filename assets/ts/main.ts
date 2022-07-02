7e325d420da1429deaf35242c17bdcfd8f160618
Pascal Wilbrink <pascalwilbrink@Pascals-MacBook-Pro.local>
2022-07-02 22:26:16 +0200

    Initial commit

---
+++ assets/ts/main.ts
@@ -0,0 +1,20 @@
+
+const app: HTMLElement = document.querySelector<HTMLElement>('h1.type')!
+
+var typewriter = new Typewriter(app, {
+    loop: true,
+    delay: 75,
+})
+  
+typewriter
+    .pauseFor(50)
+    .typeString('I\'m a Java')
+    .pauseFor(280)
+    .deleteChars(4)
+    .typeString('full-stack Java developer')
+    .pauseFor(760)
+    .deleteChars('full-stack Java developer'.length)
+    .typeString('<strong>Software</strong> developer')
+    .pauseFor(30000)
+    .start()
+  
