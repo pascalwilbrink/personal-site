6e2bf8039bec02ecba162fd56a86a0724f2aeb0c
Pascal Wilbrink <pascalwilbrink@Pascals-MacBook-Pro.local>
2022-07-03 01:05:13 +0200

    Updates

--- js/main.js
+++ js/main.js
@@ -14,3 +14,39 @@ typewriter
     .typeString('<strong>Software</strong> developer')
     .pauseFor(30000)
     .start();
+var animateCounter = function (counter) {
+    var value = Number(counter.getAttribute('data-count-number'));
+    var increment = Number(counter.getAttribute('data-count-increment')) || 1;
+    var duration = Number(counter.getAttribute('data-count-duration')) || 6000;
+    if (value) {
+        anime({
+            targets: counter,
+            textContent: value,
+            round: increment ? 1 / increment : 1 / 5,
+            easing: 'easeInOutQuad',
+            duration: duration
+        });
+    }
+};
+var counters = document.querySelectorAll('.counter');
+counters.forEach(function (counter) {
+    animateCounter(counter);
+});
+fetch('https://api.github.com/users/pascalwilbrink/events')
+    .then(function (res) {
+    return res.json();
+}).then(function (res) {
+    var pushEvents = res.filter(function (item) { return item.type === 'PushEvent'; });
+    var commitCounter = document.querySelector('.counter__commits');
+    commitCounter.setAttribute('data-count-number', pushEvents.length.toString());
+    animateCounter(commitCounter);
+});
+var calculateCodedHours = function () {
+    var yearsWorked = 7;
+    var workedDaysInYear = 200;
+    var codedHours = 8 * (workedDaysInYear * yearsWorked);
+    var codedHoursCounter = document.querySelector('.counter__coded-hours');
+    codedHoursCounter.setAttribute('data-count-number', codedHours.toString());
+    animateCounter(codedHoursCounter);
+};
+calculateCodedHours();
